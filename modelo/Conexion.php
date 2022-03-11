437bf54e3d55545ea8f5009ccd5952fe3af32b36
Eric Faustino Alvim <73504394+EricFaustino@users.noreply.github.com>
2022-03-11 15:35:18 -0300

    Update Conexion.php

--- modelo/Conexion.php
+++ modelo/Conexion.php
@@ -1,11 +1,11 @@
 <?php
 class Conexion{
     private $servidor = "us-cdbr-east-05.cleardb.net";
-    private $db = "heroku_eab186a7b269299";
+    private $db = "heroku_c21805671ad710";
     private $puerto = "3306";
     private $charset = "utf8";
-    private $usuario = "b8f91511df560c";
-    private $contrasena = "22ef1f0d";
+    private $usuario = "bfbaa7992d1a9a";
+    private $contrasena = "7dc39ec9";
     public $pdo = null;
     private $atributos = [PDO::ATTR_CASE=>PDO::CASE_LOWER,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_ORACLE_NULLS=>PDO::NULL_EMPTY_STRING,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ];
     function __construct(){
